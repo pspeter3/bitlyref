a237066e1405a150832f9bc89749c0dc3b9cb5c9
Phips Peter <pspeter333@gmail.com>
2013-07-16 09:23:54 -0700

    Adding bitly integration

--- bitlyref.py
+++ bitlyref.py
@@ -2,7 +2,7 @@
 from urllib import urlencode
 from urlparse import urlsplit, urlunsplit, parse_qs
 
-def add_referrer(uri, referrer, param='ref'):
+def add_referrer(uri, param, referrer):
   """ Adds a referrer to a url
   @parameter uri: long url to shorten
   @parameter referrer: referrer name
@@ -16,3 +16,29 @@ def add_referrer(uri, referrer, param='ref'):
   qs = urlencode(params, True)
   url_tuple = (split.scheme, split.netloc, split.path, qs, split.fragment)
   return urlunsplit(url_tuple)
+
+if __name__ == '__main__':
+  # Set up the command line options
+  from optparse import OptionParser
+  usage = 'usage: %prog [options] url referrer'
+  parser = OptionParser(usage)
+  parser.add_option('-u', '--username', help='The bit.ly username')
+  parser.add_option('-k', '--key', help='The bit.ly API key')
+  parser.add_option('-p', '--param', help='The query string parameter', default='ref')
+  (options, args) = parser.parse_args()
+  if options.username is None:
+    parser.error('Username is required')
+  if options.key is None:
+    parser.error('API Key is required. Go to https://bitly.com/a/your_api_key')
+  if len(args) is not 2:
+    parser.error('URL and Referrer are required')
+  url = args[0]
+  referrer = args[1]
+
+  # Set up bitly
+  import bitly_api
+  bitly = bitly_api.Connection(options.username, options.key)
+  referrer_url = add_referrer(url, options.param, referrer)
+  short_url = bitly.shorten(referrer_url)
+
+  print short_url['url']
--- test/test_bitlyref.py
+++ test/test_bitlyref.py
@@ -6,15 +6,15 @@ import bitlyref
 class TestBitlyRef(unittest.TestCase):
   def test_add_referrer(self):
     self.assertEqual('http://www.google.com?ref=python', 
-      bitlyref.add_referrer('http://www.google.com', 'python'))
+      bitlyref.add_referrer('http://www.google.com', 'ref', 'python'))
 
   def test_add_to_query_string(self):
     self.assertEqual('http://www.google.com?q=test&ref=python',
-      bitlyref.add_referrer('http://www.google.com?q=test', 'python'))
+      bitlyref.add_referrer('http://www.google.com?q=test', 'ref', 'python'))
 
   def test_change_parameter(self):
     self.assertEqual('http://www.google.com?trk=python', 
-      bitlyref.add_referrer('http://www.google.com', 'python', param='trk'))
+      bitlyref.add_referrer('http://www.google.com', 'trk', 'python'))
 
   def test_collision(self):
     self.assertRaises(Exception,
